d3ffdc73147dd25d73daf007cdd0cb86332c0bda
Rocean <radell93461@gmail.com>
2025-05-18 18:18:52 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+mport type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      metadata: {
+        bytecodeHash: "none", // disable ipfs
+        useLiteralContent: true, // use source code
+      },
+    },
+  },
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz",
+      chainId: 10143,
+    },
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify-api-monad.blockvision.org",
+    browserUrl: "https://testnet.monadexplorer.com",
+  },
+  // To avoid errors from Etherscan
+  etherscan: {
+    enabled: false,
+  },
 };
+
+export default config;
